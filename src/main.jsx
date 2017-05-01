832b4f0911ffec3743ce276fc673b6e6cf9a99d4
fudafoota <fudafoota@pixiv.com>
2017-05-01 18:54:36 +0900

    Mount main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/Hello';
+import Main from './components/Main';
 
-ReactDOM.render(<Hello/>, document.getElementById('root'))
+ReactDOM.render(<Main/>, document.getElementById('root'))
